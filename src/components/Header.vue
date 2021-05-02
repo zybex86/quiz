181e9b847d749e153b6e9ae52424e426ecf8a365
zybex86 <zybex1986@gmail.com>
2021-05-02 15:48:57 +0200

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>My Quiz App</b></b-nav-item>
+      <b-nav-item disabled><b>Counter: 4/10</b></b-nav-item>
+    </b-nav>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import BootstapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
+Vue.use(BootstapVue)
 Vue.config.productionTip = false
 
 new Vue({
